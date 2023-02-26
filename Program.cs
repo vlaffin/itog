4941faa32e4660687ed5a24a26daef1b53c030bf
vlad <shutikov-vlad@mail.ru>
2023-02-26 22:08:18 +0300

    первая часть

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿void MethodCreateInputArray(string[] InputArray)
+{
+    Console.Write($"Input array: ");
+    for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
+    Console.WriteLine();
+}
