ed00c103098c0bebc47bff293565692fedcb388e
vlad <shutikov-vlad@mail.ru>
2023-02-26 22:09:13 +0300

    вторая часть

--- Program.cs
+++ Program.cs
@@ -4,3 +4,17 @@
     for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
     Console.WriteLine();
 }
+void MethodCreateOutputArray(string[] OutputArray)
+{
+    Console.Write($"Output array: ");
+    string[] NewArray = new string[OutputArray.Length];
+    for (int i = 0; i < OutputArray.Length; i++)
+    {
+        if (OutputArray[i].Length <= 3)
+        {
+            NewArray[i] = OutputArray[i];
+            Console.Write($"{NewArray[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
